05f908568c3214ad4514c378fd579e07c262161b
Guilherme Bittencourt <68746811+bittenks@users.noreply.github.com>
2021-01-13 11:56:10 -0300

    1.2

--- script.js
+++ script.js
@@ -34,3 +34,12 @@ function updateCarouse() {
         "data-bg"
     )}`;
 }
+
+
+const nav = document.getElementById("nav");
+
+btn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    btn.classList.toggle("active");
+});
+
