82e6f7d1a086d783ffc608fd1f6be7fe50022a3d
Guilherme Bittencourt <68746811+bittenks@users.noreply.github.com>
2021-01-13 12:17:24 -0300

    2.0

--- script.js
+++ script.js
@@ -36,10 +36,15 @@ function updateCarouse() {
 }
 
 
+const btn = document.getElementById("btn");
 const nav = document.getElementById("nav");
 
 btn.addEventListener("click", () => {
     nav.classList.toggle("active");
     btn.classList.toggle("active");
 });
-
+nav.addEventListener("mouseleave", () =>{
+    nav.classList.toggle("active");
+    btn.classList.toggle("active");
+    
+})
